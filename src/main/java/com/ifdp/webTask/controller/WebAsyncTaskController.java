47c8f311711bda93e12f8374e1ed5b1599e05861
CMonkey <42.codemonkey@gmail.com>
2018-10-26 10:13:51 +0800

    add WebAsyncTaskController.getCompletion method

--- src/main/java/com/ifdp/webTask/controller/WebAsyncTaskController.java
+++ src/main/java/com/ifdp/webTask/controller/WebAsyncTaskController.java
@@ -1,12 +1,20 @@
 package com.ifdp.webTask.controller;
 
 import com.ifdp.webTask.service.WebAsyncTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.WebAsyncTask;
 
 import javax.annotation.Resource;
 
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+
 @RestController
 public class WebAsyncTaskController {
+    private final static Logger logger = LoggerFactory.getLogger(WebAsyncTaskController.class);
 
     private final static String ERROR_MESSAGE = "task error";
     private final static String timeout_message = "task timeout";
@@ -14,5 +22,24 @@ public class WebAsyncTaskController {
     @Resource
     WebAsyncTaskService webAsyncTaskService;
 
+    @GetMapping("/getCompletion")
+    public WebAsyncTask<String> asyncTaskCompletion(){
+        logger.info("currentThread name = {}", currentThread().getName());
+
+        WebAsyncTask<String> webAsyncTask = new WebAsyncTask<>(10 * 1000, () -> {
+            logger.info("aysnc thread = {}", currentThread().getName());
+
+            sleep(5 * 1000L);
+
+            return webAsyncTaskService.getId();
+        });
+
+        webAsyncTask.onCompletion(() -> logger.info("task finish"));
+
+        logger.info("completion end");
+
+        return webAsyncTask;
+
+    }
 
 }
